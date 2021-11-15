284a08cac31ad8dfd4d3c691c9902cfc0f6570ff
Gordon Murray <gordon@gordonmurray.com>
2021-11-15 17:05:14 +0000

    updated secrets manager to use kms key

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,7 +1,12 @@
 resource "aws_secretsmanager_secret" "example" {
+  kms_key_id              = aws_kms_key.default.key_id
   name                    = "rds_admin"
   description             = "RDS Admin password"
   recovery_window_in_days = 14
+
+  tags = {
+    Name = var.default_tag
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "secret" {
