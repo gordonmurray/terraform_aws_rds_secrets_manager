4035c51c132271532d7e33ff58f04be45d31e7ef
Gordon Murray <gordon@gordonmurray.com>
2021-11-15 17:04:52 +0000

    Use mariadb 10.4, use a t3 instance, encrypt storage, add a tag

--- rds.tf
+++ rds.tf
@@ -4,17 +4,22 @@ resource "random_password" "password" {
 }
 
 resource "aws_db_instance" "default" {
-  identifier           = "my-database"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mariadb"
-  engine_version       = "10.3.13"
-  instance_class       = "db.t2.micro"
-  username             = "Admin"
-  password             = random_password.password.result
-  parameter_group_name = "default.mariadb10.3"
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-  multi_az             = false
+  identifier              = "my-database"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "mariadb"
+  engine_version          = "10.4.21"
+  instance_class          = "db.t3.micro"
+  username                = "Admin"
+  password                = random_password.password.result
+  parameter_group_name    = "default.mariadb10.4"
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  multi_az                = false
+  storage_encrypted       = true
+  backup_retention_period = 7
 
+  tags = {
+    Name = var.default_tag
+  }
 }
