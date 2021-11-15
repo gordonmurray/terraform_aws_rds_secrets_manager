902f95a83ff21de2a6fca9ffd59898d70c8f7f28
Gordon Murray <gordon@gordonmurray.com>
2021-11-15 17:04:24 +0000

    Updated to use Terraform 0.14.10

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
+terraform {
+
+  required_version = "0.14.10"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.43.0"
+
+    }
+  }
+
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region                  = "eu-west-1"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "example"
+  profile                 = "default"
 }
